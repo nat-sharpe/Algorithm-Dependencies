6aee695dda8da54cbfa430779bc6a4bbe44dce81
Nat Sharpe <sharpe1890@gmail.com>
2018-09-24 10:07:01 -0400

    Adds Jonathan's solutions, with loops and with recursion

--- dependencies.js
+++ dependencies.js
@@ -5,7 +5,7 @@ let tasks = [
     { name: 'interview', depends: ['dinner', 'algfriday'] },
     { name: 'mop', depends: ['sweep', 'dinner'] },
     { name: 'chores', depends: ['mop', 'interview'] },
-]
+];
 
 let orderByDependencies = tasks => {
     let results = [];
@@ -28,6 +28,48 @@ let orderByDependencies = tasks => {
     };
     return results;
 };
-
 console.log(orderByDependencies(tasks))
 
+
+//Jonathan's solution:
+
+let newTasks = {
+    algfriday: [], 
+    sweep: [], 
+    interview: ['dinner', 'algfriday'], 
+    mop: ['sweep', 'dinner'], 
+    dinner: [], 
+    chores: ['mop', 'interview'] 
+}
+
+let order = (tasks) => {
+    let visited = [];
+    let taskNames = Object.keys(tasks);
+    let taskCount = taskNames.length;
+
+    for (let i = 0; i < taskCount; i++) {
+        let taskToDo = taskNames.find(task => 
+            tasks[task].every(subtask =>
+            visited.includes(subtask))
+        );
+        visited.push(taskToDo);
+        taskNames = taskNames.filter(task => task !== taskToDo);
+    };
+    return visited;
+}
+
+console.log(order(newTasks))
+
+
+let recursiveOrder = (tasks, start) => {
+    let done = new Set();
+    let doer = task => {
+        console.log(1)
+        tasks[task].forEach(subtask => doer(subtask));
+        done.add(task);
+    };
+    doer(start);
+    return done;
+};
+
+console.log(recursiveOrder(newTasks, "chores"))
